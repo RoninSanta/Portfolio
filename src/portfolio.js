db41ffe7d8dafa51acf4371bb1b1ffa100941e2d
Joseph Shen <JosephShenFan@gmail.com>
2023-10-18 20:14:03 +0800

    Portfolio Upload

---
+++ src/portfolio.js
@@ -0,0 +1,77 @@
+const header = {
+  homepage: 'https://josephshenfan.github.io',
+}
+
+const about = {
+  name: 'Joseph Shen Fan',
+  description:
+    'I am passionate about translating my creative problem-solving skills into impactful digital solutions. My journey in computer science began with diverse work experiences, where I embraced a multitude of programming languages and real-world challenges. This inspired me to embark on a formal education in Computer Science, which I graduated with First-Class Honors.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'linkedin.com/in/joseph-shen-4267a8b0',
+    github: 'https://github.com/RoninSanta',
+  },
+}
+
+const projects = [
+  {
+    name: 'JavaScript',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'React Native',
+    description:
+      'A mobile app I created in React-Native to show Bus Stops and associated Bus Information in Singapore.',
+    stack: ['API', 'JavaScript', 'React', 'Expo'],
+    sourceCode: 'https://github.com/RoninSanta/Mobile_App_Dev_API-SGBusInfoAPP',
+    livePreview:
+      'https://snack.expo.dev/@joseph_shen/fyp-busarrivalapp?platform=ios',
+  },
+  {
+    name: 'Machine Learning Text Recognition',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'C++',
+    description: 'DJ Application created using JUCE (A Audio Player GUI)',
+    stack: ['OttoDeck', 'JUCE', 'Audio Player', 'Application'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Artificial Intelligence',
+    description:
+      'Testing the effects of Evolutionary Genetic algorithm to create an object that can walk without user interference',
+    stack: ['Python', 'Genetic Algorithm', 'AI', 'Evolution'],
+    sourceCode:
+      'https://github.com/RoninSanta/Artificial_Intelligence-Genetic_Algorithms_on_Objects',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Machine Learning',
+  'Artificial Intelligence',
+  'C++',
+  'Python',
+  'Jupyter Notbook',
+  'SQL',
+]
+
+const contact = {
+  email: 'johnsmith@mail.com',
+}
+
+export { header, about, projects, skills, contact }
