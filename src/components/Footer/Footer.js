42ebaec47b6c4d76bdbaba346cdbdb7a27cf97af
Joseph Shen <JosephShenFan@gmail.com>
2023-10-19 01:09:56 +0800

    Update files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://RoninSanta.github.io/Portfolio'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Created By Joseph Shen Fan
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -5,22 +5,17 @@ const header = {
 const about = {
   name: 'Joseph Shen Fan',
   description:
-    'My journey in computer science began with diverse work experiences, where I embraced a multitude of programming languages and real-world challenges. This inspired me to embark on a formal education in Computer Science, which I graduated with First-Class Honors. I am passionate about this field and cannot wait to translate my creative skills into impactful solutions.',
-  resume: 'https://example.com',
-  social: {
-    linkedin: 'linkedin.com/in/joseph-shen-4267a8b0',
-    github: 'https://github.com/RoninSanta',
-  },
+    'My journey in computer science began with diverse past work experiences, where I was introduced the concept of programming and real-world challenges. This inspired me to embark on a formal education in Computer Science, which I graduated with First-Class Honors. I am passionate about this field and cannot wait to translate my creative skills into impactful solutions.',
+  resume: 'https://example.com'
 }
 
 const projects = [
   {
     name: 'JavaScript',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A Mario Game clone using P5 library,a JavaScript library for creative coding in graphic and interactive projects',
+    stack: ['p5.JS', 'JavaScript', 'Game Design'],
+    sourceCode: 'https://github.com/RoninSanta/Game_Project-Mario_Clone',
   },
   {
     name: 'React Native',
@@ -32,28 +27,41 @@ const projects = [
       'https://snack.expo.dev/@joseph_shen/fyp-busarrivalapp?platform=ios',
   },
   {
-    name: 'Machine Learning Text Recognition',
+    name: 'Machine Learning-Text Recognition',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Train a model that could transcribe ancient Japanese scripts into modern readable characters for researchers.',
+    stack: [
+      'Jupyter Notebook',
+      'Python',
+      'Machine Learning',
+      'KNN Classifier',
+      'Data visualization',
+    ],
+    sourceCode:
+      'https://github.com/RoninSanta/Machine_Learning-Classical_JAP_Literature_Text_Recognition',
   },
   {
     name: 'C++',
-    description: 'DJ Application created using JUCE (A Audio Player GUI)',
-    stack: ['OttoDeck', 'JUCE', 'Audio Player', 'Application'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    description: 'DJ Application created using JUCE (An Audio Player GUI)',
+    stack: ['OttoDeck', 'JUCE', 'Audio Player', 'GUI Design'],
+    sourceCode: 'https://github.com/RoninSanta/DJ_Application-JUCE_Ottodeck',
   },
   {
     name: 'Artificial Intelligence',
     description:
-      'Testing the effects of Evolutionary Genetic algorithm to create an object that can walk without user interference',
-    stack: ['Python', 'Genetic Algorithm', 'AI', 'Evolution'],
+      'Testing the effects of Evolutionary Genetic algorithm to create an object that can learn to walk without user interference',
+    stack: ['Python', 'Genetic Algorithm', 'AI'],
     sourceCode:
       'https://github.com/RoninSanta/Artificial_Intelligence-Genetic_Algorithms_on_Objects',
-    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Python',
+    description:
+      'A lightweight python script that could detect motion in camera footage, track its movement and recognize objects in the moving footage',
+    stack: ['Python', 'Numpy', 'AI'],
+    sourceCode:
+      'https://github.com/RoninSanta/CM3070-Camera-Surveillance-System-Project/tree/main',
+    livePreview: 'https://www.youtube.com/watch?v=5VUF1SD0QZ4',
   },
 ]
 
@@ -72,6 +80,10 @@ const skills = [
 
 const contact = {
   email: 'johnsmith@mail.com',
+  social: {
+    linkedin: 'linkedin.com/in/joseph-shen-4267a8b0',
+    github: 'https://github.com/RoninSanta',
+  },
 }
 
 export { header, about, projects, skills, contact }
